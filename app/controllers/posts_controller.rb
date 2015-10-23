d7318bfc427a124f270d215c0636579976367d1c
giancarlo <gsoliscastillo@gmail.com>
2015-10-22 20:21:32 -0500

    Added Post CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+before_action :find_post, only: [:show, :edit, :update, :destroy]
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: "Articulo guardado con exito"
+    else
+      render 'new', notice: "no se pudo guardar"
+    end
+  end
+
+  def show    
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update post_params
+      redirect_to @post, notice: "Se actualizo el articulo"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+end
